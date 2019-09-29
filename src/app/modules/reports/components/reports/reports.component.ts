352ff1b67cd37ec1f6094fb6420684a41208a9d5
Darwin Garcia <dgarcia@alertlogic.com>
2019-09-28 20:11:48 -0500

    changes

--- src/app/modules/reports/components/reports/reports.component.ts
+++ src/app/modules/reports/components/reports/reports.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'angular-highcharts';
 
 @Component({
   selector: 'app-reports',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
+  chart = new Chart({
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: 'Linechart'
+    },
+    credits: {
+      enabled: false
+    },
+    series: [
+      {
+        name: 'Line 1',
+        data: [1, 2, 3],
+        type: undefined
+      }
+    ]
+  });
+
+  // add point to chart serie
+  add() {
+    this.chart.addPoint(Math.floor(Math.random() * 10));
+  }
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(){
   }
 
 }
--- src/app/modules/reports/reports.module.ts
+++ src/app/modules/reports/reports.module.ts
@@ -1,13 +1,16 @@
 // Angular Modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChartModule } from 'angular-highcharts';
+
 // Components
 import { ReportsComponent } from './components/reports/reports.component';
 
 @NgModule({
   declarations: [ReportsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ChartModule // add ChartModule to your imports
   ],
   exports: [ ReportsComponent ]
 })
